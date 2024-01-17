76f8ceac8cd5414eaaa1a8922c01e3b18726933a
pratik <pratikpd00@gmail.com>
2024-01-17 15:46:59 -0500

    Remove boilerplate code

--- mandelbrot_visualizer/mandelbrot_visualizer.h
+++
@@ -1,8 +0,0 @@
-﻿// mandelbrot_visualizer.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
